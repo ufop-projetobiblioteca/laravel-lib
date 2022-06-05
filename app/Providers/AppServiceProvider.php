c2768d4d3b971df9b418afd582f30b7de24e02ed
Victor Dias <ufop.projetobiblioteca@gmail.com>
2022-06-05 11:50:45 -0300

    alterando Scheme*

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
 
         Paginator::useBootstrap();
