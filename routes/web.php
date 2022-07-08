87186c32fc53db5cb40ea0b26bbe6f0fdea4030b
Victor Dias <ufop.projetobiblioteca@gmail.com>
2022-07-08 10:45:44 -0300

    alterando rotas para produção

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ Route::resource('/emprestimos', EmprestimoController::class);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
